ab1fe5939433a0731cda21ee325d9a73ef48b41d
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-11-07 18:53:50 +0100

    add props to card from api without filter

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,13 @@
+import React from "react";
+
 function Card(props) {
   return (
     <>
-      <ul className="list">
-        <li>Name</li>
-        <li> Masculino</li>
-        <li>1984</li>
-        <li>yellow</li>
+      <ul className="list" id={props.name}>
+        <li>{props.name}</li>
+        <li>{props.gender}</li>
+        <li>{props.birth_year}</li>
+        <li>{props.eye_color}</li>
         <button className="btn details">Show details</button>
         <button className="btn save" onSubmit={props.handleSubmit}>
           Save
--- src/components/CharactersList.js
+++ src/components/CharactersList.js
@@ -10,7 +10,16 @@ function CharactersList(props) {
         <h3>Birth Year</h3>
         <h3>Eye Color</h3>
       </section>
-      <Card onSubmit={props.handleSubmit}></Card>
+      {props.characters.map((element) => (
+        <Card
+          onSubmit={props.handleSubmit}
+          key={element.name}
+          name={element.name}
+          gender={element.gender}
+          birth={element.birth_year}
+          eye={element.eye_color}
+        ></Card>
+      ))}
     </div>
   );
 }
