6170b8da3b029abbc63633f46c17ba5596334835
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-11-07 14:19:11 +0100

    add text to Saved People

--- src/components/SavedPeople.js
+++ src/components/SavedPeople.js
@@ -1,18 +1,7 @@
 function SavedPeople(props) {
   return (
     <div className="">
-      <ul
-        className="card"
-        key={props.key}
-        id={props.id}
-        onClick={props.handleFav}
-      >
-        <li>
-          <img className="image" src={props.img} alt={props.name}></img>
-        </li>
-
-        <li className="tittle">{props.name}</li>
-      </ul>
+      <p>You didn´t save any people yet</p>
     </div>
   );
 }
