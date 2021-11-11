9d14c46a1b6ac74d39ca8e257105067c14d38d68
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-11-11 17:24:38 +0100

    add css savedpeople

--- src/components/SavedPeople.js
+++ src/components/SavedPeople.js
@@ -5,6 +5,11 @@ function SavedPeople(props) {
   return (
     <div className="favorites">
       <h2 className="tittle2">Saved People</h2>
+      <div className="favorites__btn">
+        <button className="favorites__btn">All </button>
+        <button className="favorites__btn">Male </button>
+        <button className="favorites__btn">Female </button>
+      </div>
       <div className="favorites__list">
         <Cardfav characterFav={props.characterFav}></Cardfav>
       </div>
