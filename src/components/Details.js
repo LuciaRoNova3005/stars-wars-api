edbc742082f8690f57fb8ddbe255d098ddb3b79d
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-12-16 13:36:33 +0100

    Add styleshhet details adn delete information about

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,24 +1,24 @@
 function Details(props) {
   return (
-    <article>
-      <p className="information__text">Name:{props.characterDetail.name}</p>
-      <p className="information__text">Gender:{props.characterDetail.gender}</p>
-      <p className="information__text">Height:{props.characterDetail.height}</p>
-      <p className="information__text">Mass:{props.characterDetail.mass}</p>
-      <p className="information__text">Mass:{props.characterDetail.mass}</p>
-      <p className="information__text">
+    <article className="cardDetails">
+      <p className="cardDetails__text">Name:{props.characterDetail.name}</p>
+      <p className="cardDetails__text">Gender:{props.characterDetail.gender}</p>
+      <p className="cardDetails__text">Height:{props.characterDetail.height}</p>
+      <p className="cardDetails__text">Mass:{props.characterDetail.mass}</p>
+      <p className="cardDetails__text">Mass:{props.characterDetail.mass}</p>
+      <p className="cardDetails__text">
         Skin color:{props.characterDetail.skin_color}
       </p>
-      <p className="information__text">
+      <p className="cardDetails__text">
         Hair color:{props.characterDetail.hair_color}
       </p>
-      <p className="information__text">
+      <p className="cardDetails__text">
         Eyes colors:{props.characterDetail.birth_year}
       </p>
-      <p className="information__text">
+      <p className="cardDetails__text">
         Birth year:{props.characterDetail.birth_year}
       </p>
-      <p className="information__text">
+      <p className="cardDetails__text">
         Hair color:{props.characterDetail.birth_year}
       </p>
       <button
