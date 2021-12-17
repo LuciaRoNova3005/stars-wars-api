35e73175f1121d02c8b90d77076535a0b3094032
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-12-17 09:46:07 +0100

    Fixed details props

--- src/components/Details.js
+++ src/components/Details.js
@@ -14,12 +14,8 @@ function Details(props) {
         <li className="cardDetails__text">
           Height: {props.characterDetail.height}
         </li>
-        <li className="cardDetails__text">Mass:{props.characterDetail.mass}</li>
         <li className="cardDetails__text">
           Skin color: {props.characterDetail.skin_color}
-        </li>
-        <li className="cardDetails__text">
-          Hair color:{props.characterDetail.hair_color}
         </li>
         <li className="cardDetails__text">
           Eyes colors: {props.characterDetail.eye_color}
