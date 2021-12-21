135c1761c59dc67af866759458a734c9b88240ba
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-12-21 13:42:03 +0100

    Add delte component

--- src/components/NotCharacertersFound.js
+++
@@ -1,9 +0,0 @@
-const NotCharactersFound = (props) => {
-  return (
-    <section className="">
-      <p className="text">There are not characters that match your search</p>
-    </section>
-  );
-};
-
-export default NotCharactersFound;
