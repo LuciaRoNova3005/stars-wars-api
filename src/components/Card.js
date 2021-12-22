5f3bcce113d0efac9ad928b135294cd4d71af572
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-12-22 11:21:04 +0100

    Fixed card styleshets

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Card(props) {
   return (
-    <article className="card" id={props.id} key={props.key}>
+    <article className="elements__card" id={props.id} key={props.key}>
       <div className="card__information">
         <p>Name: {props.name}</p>
         <p>Gender: {props.gender}</p>
@@ -17,6 +17,7 @@ function Card(props) {
         >
           Show details
         </button>
+
         <button className="btn save" id={props.name} onClick={props.handleFav}>
           Saved
         </button>
